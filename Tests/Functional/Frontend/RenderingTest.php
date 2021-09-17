2ce8efeed41ee2bdc8639cf0a2c64daf7967ceea
Maik Kempe <mkempe@bitaculous.com>
2021-09-17 20:21:00 +0200

    [✓] Fixed rendering test.

--- Tests/Functional/Frontend/RenderingTest.php
+++ Tests/Functional/Frontend/RenderingTest.php
@@ -35,8 +35,13 @@ class RenderingTest extends FunctionalTestCase
      * @var array
      */
     protected $testExtensionsToLoad = [
-        'typo3conf/ext/t3v_testing',
-        'typo3conf/ext/t3v_base'
+        'typo3conf/ext/t3v_base',
+        'typo3conf/ext/gridelements',
+        'typo3conf/ext/sms_responsive_images',
+        'typo3conf/ext/vhs',
+        'typo3conf/ext/t3v_core',
+        'typo3conf/ext/t3v_translations',
+        'typo3conf/ext/t3v_testing'
     ];
 
     /**
