258cc9b426cf5b88e2947eda7bb5238edae4c214
Maik Kempe <mkempe@bitaculous.com>
2021-10-01 17:27:12 +0200

    [♻] Cleaned up.

--- ext_localconf.php
+++ ext_localconf.php
@@ -19,7 +19,7 @@ defined('TYPO3_MODE') or die();
     // === TSconfig ===
 
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-        "<INCLUDE_TYPOSCRIPT: source=\"{$tsConfigFolder}/Page.tsconfig\">"
+        "<INCLUDE_TYPOSCRIPT: source=\"$tsConfigFolder/Page.tsconfig\">"
     );
 
     // === Content Objects ===
@@ -191,31 +191,31 @@ defined('TYPO3_MODE') or die();
 
         $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
         $icons = [
-            '1 Column Layout Grid Element' => "{$iconsFolder}/GridElements/ColumnLayoutGridElement/1Column.svg",
-            '2 Column Layout Grid Element' => "{$iconsFolder}/GridElements/ColumnLayoutGridElement/2Column.svg",
-            '3 Column Layout Grid Element' => "{$iconsFolder}/GridElements/ColumnLayoutGridElement/3Column.svg",
-            '4 Column Layout Grid Element' => "{$iconsFolder}/GridElements/ColumnLayoutGridElement/4Column.svg",
-            'Background' => "{$iconsFolder}/ContentObjects/Background.svg",
-            'Bar Grid Element' => "{$iconsFolder}/GridElements/BarGridElement.svg",
-            'Block Grid Element' => "{$iconsFolder}/GridElements/BlockGridElement.svg",
-            'Cascading Layout Grid Element' => "{$iconsFolder}/GridElements/CascadingLayoutGridElement.svg",
-            'Conditionally Grid Element' => "{$iconsFolder}/GridElements/ConditionallyGridElement.svg",
-            'Container Grid Element' => "{$iconsFolder}/GridElements/ContainerGridElement.svg",
-            'Copyright' => "{$iconsFolder}/ContentObjects/Copyright.svg",
-            'Flexbox Grid Element' => "{$iconsFolder}/GridElements/FlexboxGridElement.svg",
-            'Flyout Grid Element' => "{$iconsFolder}/GridElements/FlyoutGridElement.svg",
-            'Footer Grid Element' => "{$iconsFolder}/GridElements/FooterGridElement.svg",
-            'Formatting Grid Element' => "{$iconsFolder}/GridElements/FormattingGridElement.svg",
-            'Header Grid Element' => "{$iconsFolder}/GridElements/HeaderGridElement.svg",
-            'Icon' => "{$iconsFolder}/ContentObjects/Icon.svg",
-            'Label' => "{$iconsFolder}/ContentObjects/Label.svg",
-            'Link' => "{$iconsFolder}/ContentObjects/Link.svg",
-            'Logo' => "{$iconsFolder}/ContentObjects/Logo.svg",
-            'Modal Grid Element' => "{$iconsFolder}/GridElements/ModalGridElement.svg",
-            'Section Grid Element' => "{$iconsFolder}/GridElements/SectionGridElement.svg",
-            'Search Bar' => "{$iconsFolder}/ContentObjects/SearchBar.svg",
-            'Spacer' => "{$iconsFolder}/ContentObjects/Spacer.svg",
-            'Viewport Grid Element' => "{$iconsFolder}/GridElements/ViewportGridElement.svg"
+            '1 Column Layout Grid Element' => "$iconsFolder/GridElements/ColumnLayoutGridElement/1Column.svg",
+            '2 Column Layout Grid Element' => "$iconsFolder/GridElements/ColumnLayoutGridElement/2Column.svg",
+            '3 Column Layout Grid Element' => "$iconsFolder/GridElements/ColumnLayoutGridElement/3Column.svg",
+            '4 Column Layout Grid Element' => "$iconsFolder/GridElements/ColumnLayoutGridElement/4Column.svg",
+            'Background' => "$iconsFolder/ContentObjects/Background.svg",
+            'Bar Grid Element' => "$iconsFolder/GridElements/BarGridElement.svg",
+            'Block Grid Element' => "$iconsFolder/GridElements/BlockGridElement.svg",
+            'Cascading Layout Grid Element' => "$iconsFolder/GridElements/CascadingLayoutGridElement.svg",
+            'Conditionally Grid Element' => "$iconsFolder/GridElements/ConditionallyGridElement.svg",
+            'Container Grid Element' => "$iconsFolder/GridElements/ContainerGridElement.svg",
+            'Copyright' => "$iconsFolder/ContentObjects/Copyright.svg",
+            'Flexbox Grid Element' => "$iconsFolder/GridElements/FlexboxGridElement.svg",
+            'Flyout Grid Element' => "$iconsFolder/GridElements/FlyoutGridElement.svg",
+            'Footer Grid Element' => "$iconsFolder/GridElements/FooterGridElement.svg",
+            'Formatting Grid Element' => "$iconsFolder/GridElements/FormattingGridElement.svg",
+            'Header Grid Element' => "$iconsFolder/GridElements/HeaderGridElement.svg",
+            'Icon' => "$iconsFolder/ContentObjects/Icon.svg",
+            'Label' => "$iconsFolder/ContentObjects/Label.svg",
+            'Link' => "$iconsFolder/ContentObjects/Link.svg",
+            'Logo' => "$iconsFolder/ContentObjects/Logo.svg",
+            'Modal Grid Element' => "$iconsFolder/GridElements/ModalGridElement.svg",
+            'Section Grid Element' => "$iconsFolder/GridElements/SectionGridElement.svg",
+            'Search Bar' => "$iconsFolder/ContentObjects/SearchBar.svg",
+            'Spacer' => "$iconsFolder/ContentObjects/Spacer.svg",
+            'Viewport Grid Element' => "$iconsFolder/GridElements/ViewportGridElement.svg"
         ];
 
         foreach ($icons as $name => $source) {
