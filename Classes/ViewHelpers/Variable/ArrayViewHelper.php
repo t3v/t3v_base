9684505823e8ad415f31745fc8586ecd9959cdb0
Maik Kempe <mkempe@bitaculous.com>
2022-04-28 16:06:57 +0200

    [✚] Added array and default view helper from T3v Core.

---
+++ Classes/ViewHelpers/Variable/ArrayViewHelper.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+namespace T3v\T3vBase\ViewHelpers\Variable;
+
+use T3v\T3vCore\ViewHelpers\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+
+/**
+ * The array view helper class.
+ *
+ * @package T3v\T3vBase\ViewHelpers\Variable
+ */
+class ArrayViewHelper extends AbstractViewHelper
+{
+    /**
+     * Initializes the arguments.
+     */
+    public function initializeArguments(): void
+    {
+        parent::initializeArguments();
+
+        $this->registerArgument('array', 'array', 'The array', true);
+        $this->registerArgument('key', 'string', 'The key', true);
+    }
+
+    /**
+     * The view helper render static function.
+     *
+     * @param array $arguments The arguments
+     * @param \Closure $renderChildrenClosure The render children closure
+     * @param RenderingContextInterface $renderingContext The rendering context
+     * @return object|null The value for the key or null if the key does not exist
+     * @noinspection PhpFullyQualifiedNameUsageInspection
+     */
+    public static function renderStatic(
+        array $arguments,
+        \Closure $renderChildrenClosure,
+        RenderingContextInterface $renderingContext
+    ): ?object {
+        $result = null;
+        $array = $arguments['array'];
+        $key = $arguments['key'];
+
+        if (is_array($array) && $key && array_key_exists($key, $array)) {
+            $result = $array[$key];
+        }
+
+        return $result;
+    }
+}
---
+++ Classes/ViewHelpers/Variable/DefaultViewHelper.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace T3v\T3vBase\ViewHelpers\Variable;
+
+use T3v\T3vCore\ViewHelpers\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+
+/**
+ * The default view helper class.
+ *
+ * @package T3v\T3vBase\ViewHelpers\Variable
+ */
+class DefaultViewHelper extends AbstractViewHelper
+{
+    /**
+     * Initializes the arguments.
+     */
+    public function initializeArguments(): void
+    {
+        parent::initializeArguments();
+        $this->registerArgument('value', 'string', 'The value');
+        $this->registerArgument('default', 'string', 'The default value, defaults to `-`', false, '-');
+    }
+
+    /**
+     * The view helper render static function.
+     *
+     * @param array $arguments The arguments
+     * @param \Closure $renderChildrenClosure The render children closure
+     * @param RenderingContextInterface $renderingContext The rendering context
+     * @return string The value if available, otherwise the default one
+     * @noinspection PhpFullyQualifiedNameUsageInspection
+     */
+    public static function renderStatic(
+        array $arguments,
+        \Closure $renderChildrenClosure,
+        RenderingContextInterface $renderingContext
+    ): string {
+        return $arguments['value'] ?: $arguments['default'];
+    }
+}
