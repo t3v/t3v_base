64f12f6762577983dd72472fecaea8f2f06014e4
Maik Kempe <mkempe@bitaculous.com>
2022-05-27 18:24:26 +0200

    [➠] Added `Variable` view helper.

---
+++ Classes/ViewHelpers/VariableViewHelper.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace T3v\T3vBase\ViewHelpers;
+
+use TYPO3Fluid\Fluid\ViewHelpers\VariableViewHelper as FluidVariableViewHelper;
+
+/**
+ * The variable view helper class.
+ *
+ * @package T3v\T3vBase\ViewHelpers
+ */
+class VariableViewHelper extends FluidVariableViewHelper
+{
+}
