6dae9891dd446887d72d2903bd8a55c8c80251ee
Maik Kempe <mkempe@bitaculous.com>
2022-06-14 20:13:25 +0200

    [➠] Added sum view helper.

---
+++ Classes/ViewHelpers/Math/SumViewHelper.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace T3v\T3vBase\ViewHelpers\Math;
+
+use FluidTYPO3\Vhs\ViewHelpers\Math\SumViewHelper as VhsSumViewHelper;
+
+/**
+ * The sum view helper class.
+ *
+ * @package T3v\T3vBase\ViewHelpers\Format
+ */
+class SumViewHelper extends VhsSumViewHelper
+{
+}
