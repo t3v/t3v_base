25c3e18f1775b3b5970a95d6993e21274078b1b6
Maik Kempe <mkempe@bitaculous.com>
2022-06-14 20:36:16 +0200

    [✚] Added subtract and bread crumb view helper.

---
+++ Classes/ViewHelpers/Math/SubtractViewHelper.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace T3v\T3vBase\ViewHelpers\Math;
+
+use FluidTYPO3\Vhs\ViewHelpers\Math\SubtractViewHelper as VhsSubtractViewHelper;
+
+/**
+ * The subtract view helper class.
+ *
+ * @package T3v\T3vBase\ViewHelpers\Format
+ */
+class SubtractViewHelper extends VhsSubtractViewHelper
+{
+}
