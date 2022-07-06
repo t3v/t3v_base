24b29c220ecfabcb73794961cc76d5c313c58004
Maik Kempe <mkempe@bitaculous.com>
2022-07-06 19:56:10 +0200

    [➠] Added `Explode` view helper.

---
+++ Classes/ViewHelpers/Iterator/ExplodeViewHelper.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace T3v\T3vBase\ViewHelpers\Iterator;
+
+use FluidTYPO3\Vhs\ViewHelpers\Iterator\ExplodeViewHelper as VhsExplodeViewHelper;
+
+/**
+ * The explode view helper class.
+ *
+ * @package T3v\T3vBase\ViewHelpers\Iterator
+ */
+class ExplodeViewHelper extends VhsExplodeViewHelper
+{
+}
