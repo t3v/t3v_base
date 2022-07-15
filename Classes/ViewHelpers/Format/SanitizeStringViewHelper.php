10322805261aa7b635dfe88f65f30bfdab2c46d8
Maik Kempe <mkempe@bitaculous.com>
2022-07-15 17:03:27 +0200

    [✚] Added more view helpers.

---
+++ Classes/ViewHelpers/Format/SanitizeStringViewHelper.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace T3v\T3vBase\ViewHelpers\Format;
+
+use FluidTYPO3\Vhs\ViewHelpers\Format\SanitizeStringViewHelper as VhsSanitizeStringViewHelper;
+
+/**
+ * The sanitize string view helper class.
+ *
+ * @package T3v\T3vBase\ViewHelpers\Format
+ */
+class SanitizeStringViewHelper extends VhsSanitizeStringViewHelper
+{
+}
---
+++ Classes/ViewHelpers/Menu/ListViewHelper.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace T3v\T3vBase\ViewHelpers\Menu;
+
+use FluidTYPO3\Vhs\ViewHelpers\Menu\ListViewHelper as VhsListViewHelper;
+
+/**
+ * The list view helper class.
+ *
+ * @package T3v\T3vBase\ViewHelpers\Menu
+ */
+class ListViewHelper extends VhsListViewHelper
+{
+}
