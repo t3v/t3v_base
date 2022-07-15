3da43511aaefe3a8af2625d0a30ec04e64d8f3d4
Maik Kempe <mkempe@bitaculous.com>
2022-07-15 13:18:31 +0200

    [✚] Added substring view helper.

---
+++ Classes/ViewHelpers/Format/SubstringViewHelper.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace T3v\T3vBase\ViewHelpers\Format;
+
+use FluidTYPO3\Vhs\ViewHelpers\Format\SubstringViewHelper as VhsSubstringViewHelper;
+
+/**
+ * The substring view helper class.
+ *
+ * @package T3v\T3vBase\ViewHelpers\Format
+ */
+class SubstringViewHelper extends VhsSubstringViewHelper
+{
+}
