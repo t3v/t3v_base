a319724a7b65a6d514717f26802704896f3ad06d
Maik Kempe <mkempe@bitaculous.com>
2022-08-24 13:45:45 +0200

    [➠] Added alias view helper.

---
+++ Classes/ViewHelpers/AliasViewHelper.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace T3v\T3vBase\ViewHelpers;
+
+use TYPO3Fluid\Fluid\ViewHelpers\AliasViewHelper as FluidAliasViewHelper;
+
+/**
+ * The alias view helper class.
+ *
+ * @package T3v\T3vBase\ViewHelpers
+ */
+class AliasViewHelper extends FluidAliasViewHelper
+{
+}
