a36c4f7be98fbbcca9eaf2f86033ec6877e2becf
Maik Kempe <mkempe@bitaculous.com>
2022-09-27 17:52:50 +0200

    [✚] Added iterator filter view helper.

---
+++ Classes/ViewHelpers/Iterator/FilterViewHelper.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace T3v\T3vBase\ViewHelpers\Iterator;
+
+use FluidTYPO3\Vhs\ViewHelpers\Iterator\FilterViewHelper as VhsFilterViewHelper;
+
+/**
+ * The filter view helper class.
+ *
+ * @package T3v\T3vBase\ViewHelpers\Iterator
+ */
+class FilterViewHelper extends VhsFilterViewHelper
+{
+}
