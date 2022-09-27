ec6ddd3e0cbae7601d1739077c45f237d3738dc3
Maik Kempe <mkempe@bitaculous.com>
2022-09-27 17:50:34 +0200

    [✚] Added page language menu view helper.

---
+++ Classes/ViewHelpers/Page/LanguageMenuViewHelper.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace T3v\T3vBase\ViewHelpers\Page;
+
+use FluidTYPO3\Vhs\ViewHelpers\Page\LanguageMenuViewHelper as VhsLanguageMenuViewHelper;
+
+/**
+ * The language menu view helper class.
+ *
+ * @package T3v\T3vBase\ViewHelpers\Format
+ */
+class LanguageMenuViewHelper extends VhsLanguageMenuViewHelper
+{
+}
