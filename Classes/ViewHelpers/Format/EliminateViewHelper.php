94cee3aba1c30dfcac3160e46d05aee32b3d0478
Maik Kempe <mkempe@bitaculous.com>
2023-04-25 23:28:15 +0200

    [✚] Added format eliminate view helper.

---
+++ Classes/ViewHelpers/Format/EliminateViewHelper.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace T3v\T3vBase\ViewHelpers\Format;
+
+use FluidTYPO3\Vhs\ViewHelpers\Format\EliminateViewHelper as VhsEliminateViewHelper;
+
+/**
+ * The eliminate view helper class.
+ *
+ * @package T3v\T3vBase\ViewHelpers\Format
+ */
+class EliminateViewHelper extends VhsEliminateViewHelper
+{
+}
